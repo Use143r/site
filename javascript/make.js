4030eb3f01968a867ce9398bb85acf9cc4b8a4b1
Use143r <93150584+Use143r@users.noreply.github.com>
2022-11-27 17:25:50 +0300

    Add files via upload

---
+++ javascript/make.js
@@ -0,0 +1,15 @@
+var today = new Date ();
+var hourNow = today.getHours ();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Вечер добрый! пидарас';
+}   
+else if (hourNow >  12 ) {
+    greeting = 'ДОбРый день, козлина!';
+}   
+else if (hourNow >  2 ) {
+    greeting = 'Хули заперся в такое время?';
+}  
+
+document.write ('<h3>' + greeting + '</h3>');
